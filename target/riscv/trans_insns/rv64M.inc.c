222846ffc13d0df2410c4239b187529a89c36d4a
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2018-03-14 18:42:19 +0100

    Add RV64M instructions
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

---
+++ target/riscv/trans_insns/rv64M.inc.c
@@ -0,0 +1,72 @@
+bool trans_MULW(DisasContext *ctx, arg_MULW *a, uint32_t insn)
+{
+#if defined(TARGET_RISCV64)
+    LOAD_ARGS
+    tcg_gen_mul_tl(source1, source1, source2);
+    RESULT_AND_FREE
+    return true;
+#else
+    gen_exception_illegal(ctx);
+    return false;
+#endif
+}
+
+static bool trans_DIVW(DisasContext *ctx, arg_DIVW *a, uint32_t insn)
+{
+#if defined(TARGET_RISCV64)
+    LOAD_ARGS
+    tcg_gen_ext32s_tl(source1, source1);
+    tcg_gen_ext32s_tl(source2, source2);
+    gen_div(source1, source1, source2);
+    RESULT_AND_FREE
+    return true;
+#else
+    gen_exception_illegal(ctx);
+    return false;
+#endif
+}
+
+static bool trans_DIVUW(DisasContext *ctx, arg_DIVUW *a, uint32_t insn)
+{
+#if defined(TARGET_RISCV64)
+    LOAD_ARGS
+    tcg_gen_ext32u_tl(source1, source1);
+    tcg_gen_ext32u_tl(source2, source2);
+    gen_divu(source1, source1, source2);
+    RESULT_AND_FREE
+    return true;
+#else
+    gen_exception_illegal(ctx);
+    return false;
+#endif
+}
+
+static bool trans_REMW(DisasContext *ctx, arg_REMW *a, uint32_t insn)
+{
+#if defined(TARGET_RISCV64)
+    LOAD_ARGS
+    tcg_gen_ext32s_tl(source1, source1);
+    tcg_gen_ext32s_tl(source2, source2);
+    gen_rem(source1, source1, source2);
+    RESULT_AND_FREE
+    return true;
+#else
+    gen_exception_illegal(ctx);
+    return false;
+#endif
+}
+
+static bool trans_REMUW(DisasContext *ctx, arg_REMUW *a, uint32_t insn)
+{
+#if defined(TARGET_RISCV64)
+    LOAD_ARGS
+    tcg_gen_ext32u_tl(source1, source1);
+    tcg_gen_ext32u_tl(source2, source2);
+    gen_remu(source1, source1, source2);
+    RESULT_AND_FREE
+    return true;
+#else
+    gen_exception_illegal(ctx);
+    return false;
+#endif
+}
